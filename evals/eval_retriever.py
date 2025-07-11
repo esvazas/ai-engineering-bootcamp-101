28f19546a9be06dfe07685b8205d6b4f1b842393
Erikas Švažas <erikas.svazas@vinted.com>
2025-07-11 23:17:50 +0300

    Fix evals and move notebook

--- evals/eval_retriever.py
+++ evals/eval_retriever.py
@@ -1,93 +1,98 @@
 import os
+
+from chatbot_ui.core.config import config
+from chatbot_ui.retrieval import rag_pipeline
+
 from langsmith import Client
+from qdrant_client import QdrantClient
 from langchain_openai import ChatOpenAI
 from langchain_openai import OpenAIEmbeddings
+
 from ragas.llms import LangchainLLMWrapper
 from ragas.embeddings import LangchainEmbeddingsWrapper
-from ragas.dataset_schema import SingleTurnSample
-from ragas.metrics import Faithfulness, ResponseRelevancy, ContextPrecision, NonLLMContextRecall, ContextRecall
-from qdrant_client import QdrantClient
-
-
-from chatbot_ui.retrieval import rag_pipeline
-from chatbot_ui.core.config import config
 
 
 ls_client = Client(api_key=config.LANGSMITH_API_KEY)
-qdrant_client = QdrantClient(url="http://localhost:6333")
-
-
-ragas_llm = LangchainLLMWrapper(
-    ChatOpenAI(
-        model='gpt-4.1'
-    )
-)
-ragas_embeddings = LangchainEmbeddingsWrapper(
-    OpenAIEmbeddings(
-        model='text-embedding-3-small'
-    )
+qdrant_client = QdrantClient(
+    url=f"http://localhost:6333"
 )
 
+from ragas.dataset_schema import SingleTurnSample
+from ragas.metrics import Faithfulness, ResponseRelevancy, LLMContextPrecisionWithoutReference, LLMContextRecall, NonLLMContextRecall
 
-async def ragas_response_relevance(run, example):
+ragas_llm = LangchainLLMWrapper(ChatOpenAI(model="gpt-4.1"))
+ragas_embeddings = LangchainEmbeddingsWrapper(OpenAIEmbeddings(model="text-embedding-3-small"))
+
+
+async def ragas_faithfulness(run, example):
+
     sample = SingleTurnSample(
-        user_input=run.outputs['question'],
-        response=run.outputs['answer'],
-        retrieved_contexts=run.outputs['retrieved_context']
-    )
+            user_input=run.outputs["question"],
+            response=run.outputs["answer"],
+            retrieved_contexts=run.outputs["retrieved_context"]
+        )
+    scorer = Faithfulness(llm=ragas_llm)
+
+    return await scorer.single_turn_ascore(sample)
+
+
+async def ragas_responce_relevancy(run, example):
+
+    sample = SingleTurnSample(
+            user_input=run.outputs["question"],
+            response=run.outputs["answer"],
+            retrieved_contexts=run.outputs["retrieved_context"]
+        )
     scorer = ResponseRelevancy(llm=ragas_llm, embeddings=ragas_embeddings)
+
     return await scorer.single_turn_ascore(sample)
 
 
 async def ragas_context_precision(run, example):
+
     sample = SingleTurnSample(
-        user_input=run.outputs['question'],
-        response=run.outputs['answer'],
-        retrieved_contexts=run.outputs['retrieved_context']
-    )
-    scorer = ContextPrecision(llm=ragas_llm)
+            user_input=run.outputs["question"],
+            response=run.outputs["answer"],
+            retrieved_contexts=run.outputs["retrieved_context"]
+        )
+    scorer = LLMContextPrecisionWithoutReference(llm=ragas_llm)
+
     return await scorer.single_turn_ascore(sample)
 
 
 async def ragas_context_recall_llm_based(run, example):
+
     sample = SingleTurnSample(
-        user_input=run.outputs['question'],
-        response=run.outputs['answer'],
-        reference=example.outputs['ground_truth'],
-        retrieved_contexts=run.outputs['retrieved_context']
-    )
-    scorer = ContextRecall(llm=ragas_llm)
+            user_input=run.outputs["question"],
+            response=run.outputs["answer"],
+            reference=example.outputs["ground_truth"],
+            retrieved_contexts=run.outputs["retrieved_context"]
+        )
+    scorer = LLMContextRecall(llm=ragas_llm)
+
     return await scorer.single_turn_ascore(sample)
 
 
-async def ragas_context_recall_non_llm_based(run, example):
+async def ragas_context_recall_non_llm(run, example):
+
     sample = SingleTurnSample(
-        reference_contexts=example.outputs['contexts'],
-        retrieved_contexts=run.outputs['retrieved_context']
-    )
+            retrieved_contexts=run.outputs["retrieved_context"],
+            reference_contexts=example.outputs["contexts"]
+        )
     scorer = NonLLMContextRecall()
+
     return await scorer.single_turn_ascore(sample)
-
-
-async def ragas_faithfulness(run, example):
-    sample = SingleTurnSample(
-        user_input=run.outputs['question'],
-        response=run.outputs['answer'],
-        retrieved_contexts=run.outputs['retrieved_context']
-    )
-    scorer = Faithfulness(llm=ragas_llm)
-    await scorer.single_turn_ascore(sample)
 
 
 results = ls_client.evaluate(
-    lambda x: rag_pipeline(x['question'], qdrant_client),
-    data='rag-evaluation-dataset',
+    lambda x: rag_pipeline(x["question"], qdrant_client),
+    data="rag-evaluation-dataset",
     evaluators=[
-        ragas_response_relevance,
+        ragas_faithfulness,
+        ragas_responce_relevancy,
         ragas_context_precision,
         ragas_context_recall_llm_based,
-        ragas_context_recall_non_llm_based,
-        ragas_faithfulness
+        ragas_context_recall_non_llm
     ],
-    experiment_prefix='rag-evaluation-dataset'
+    experiment_prefix="rag-evaluation-dataset"
 )
--- src/chatbot_ui/retrieval.py
+++ src/chatbot_ui/retrieval.py
@@ -1,36 +1,41 @@
 import openai
-from langsmith import traceable, get_current_run_tree
+
 from qdrant_client import QdrantClient
+from langsmith import traceable, get_current_run_tree
 
 from chatbot_ui.core.config import config
 
 
 @traceable(
-    name='embed_query',
-    run_type='embedding',
-    metadata={'ls_provider': config.EMBEDDING_MODEL_PROVIDER,
-    'ls_model_name': config.EMBEDDING_MODEL}
-    )
-def get_embeddings(text, model=config.EMBEDDING_MODEL):
+    name="embed_query",
+    run_type="embedding",
+    metadata={"ls_provider": config.EMBEDDING_MODEL_PROVIDER, "ls_model_name": config.EMBEDDING_MODEL}
+)
+def get_embedding(text, model=config.EMBEDDING_MODEL):
     response = openai.embeddings.create(
         input=[text],
         model=model,
     )
 
     current_run = get_current_run_tree()
-    current_run.metadata['usage_metadata'] = {
-        'input_tokens': response.usage.prompt_tokens,
-        'total_tokens': response.usage.total_tokens,
-    }
+    if current_run:
+        current_run.metadata["usage_metadata"] = {
+            "input_tokens": response.usage.prompt_tokens,
+            "total_tokens": response.usage.total_tokens,
+        }
 
     return response.data[0].embedding
 
-@traceable(name='retrieve_top_n', run_type='retriever')
+
+@traceable(
+    name="retrieve_top_n",
+    run_type="retriever"
+)
 def retrieve_context(query, qdrant_client, top_k=5):
-    query_embeddings = get_embeddings(query)
+    query_embedding = get_embedding(query)
     results = qdrant_client.query_points(
-        collection_name=f"{config.QDRANT_COLLECTION_NAME}",
-        query=query_embeddings,
+        collection_name=config.QDRANT_COLLECTION_NAME,
+        query=query_embedding,
         limit=top_k,
     )
 
@@ -44,46 +49,58 @@ def retrieve_context(query, qdrant_client, top_k=5):
         similarity_scores.append(result.score)
 
     return {
-        'retrieved_context_ids': retrieved_context_ids,
-        'retrieved_context': retrieved_context,
-        'similarity_scores': similarity_scores,
+        "retrieved_context_ids": retrieved_context_ids,
+        "retrieved_context": retrieved_context,
+        "similarity_scores": similarity_scores
     }
 
-@traceable(name='format_retrieved_context', run_type='prompt')
+
+@traceable(
+    name="format_retrieved_context",
+    run_type="prompt"
+)
 def process_context(context):
+
     formatted_context = ""
-    for chunk in context['retrieved_context']:
+
+    for chunk in context["retrieved_context"]:
         formatted_context += f"- {chunk}\n"
+
     return formatted_context
 
 
-@traceable(name='render_prompt', run_type='prompt')
-def buildpromot(context, question):
+@traceable(
+    name="render_prompt",
+    run_type="prompt"
+)
+def build_prompt(context, question):
+
     processed_context = process_context(context)
+
     prompt = f"""
-    You are a helpful shoppingassistant that can answer questions about the products in stock:
-    You will be given a question and a list of context.
+You are a shopping assistant that can answer questions about the products in stock.
 
-    Instructions:
-    - You need to answer the question based on the provided context only
-    - Never use word context and refer to it as the available products
+You will be given a question and a list of context.
 
-    Context:
-    {processed_context}
+Instructtions:
+- You need to answer the question based on the provided context only.
+- Never use word context and refer to it as the available products.
 
-    Question:
-    {question}
-    """
+Context:
+{processed_context}
+
+Question:
+{question}
+"""
+
     return prompt
 
+
 @traceable(
-    name='generate_answer',
-    run_type='llm',
-    metadata={
-        'ls_provider': config.GENERATION_MODEL_PROVIDER,
-        'ls_model_name': config.GENERATION_MODEL
-        }
-    )
+    name="generate_answer",
+    run_type="llm",
+    metadata={"ls_provider": config.GENERATION_MODEL_PROVIDER, "ls_model_name": config.GENERATION_MODEL}
+)
 def generate_answer(prompt):
     response = openai.chat.completions.create(
         model="gpt-4.1",
@@ -92,26 +109,31 @@ def generate_answer(prompt):
     )
 
     current_run = get_current_run_tree()
-    current_run.metadata['usage_metadata'] = {
-        'input_tokens': response.usage.prompt_tokens,
-        'output_tokens': response.usage.completion_tokens,
-        'total_tokens': response.usage.total_tokens,
-    }
+    if current_run:
+        current_run.metadata["usage_metadata"] = {
+            "input_tokens": response.usage.prompt_tokens,
+            "output_tokens": response.usage.completion_tokens,
+            "total_tokens": response.usage.total_tokens,
+        }
 
     return response.choices[0].message.content
 
-@traceable(name='rag_pipeline')
+
+@traceable(
+    name="rag_pipeline",
+)
 def rag_pipeline(question, qdrant_client, top_k=5):
 
     retrieved_context = retrieve_context(question, qdrant_client, top_k)
-    prompt = buildpromot(retrieved_context, question)
+    prompt = build_prompt(retrieved_context, question)
     answer = generate_answer(prompt)
 
-    return {
-        'answer': answer,
-        'question': question,
-        'retrieved_context_ids': retrieved_context['retrieved_context_ids'],
-        'similarity_scores': retrieved_context['similarity_scores'],
-        'retrieved_context': retrieved_context['retrieved_context'],
+    final_result = {
+        "answer": answer,
+        "question": question,
+        "retrieved_context_ids": retrieved_context["retrieved_context_ids"],
+        "retrieved_context": retrieved_context["retrieved_context"],
+        "similarity_scores": retrieved_context["similarity_scores"]
     }
 
+    return final_result
